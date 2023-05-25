c5a495a67c84a336ff88788429d9617a8f4e6c27
Nathymmd <nathycmmd2001@gmail.com>
2023-05-25 02:19:12 +0200

    envio proyecto n1

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Servidor corriendo en el puerto 3000')
+})
+
+/*app.listen (3000, () => {
+    console.log('el servidor esta activo');
+});*/
+
+
+app.use(express.static('public'));
+
+const path = require('path'); //resolver obtener la ruta absoluta
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'/views/home.html'));
+});
+app.get('/registro', (req, res) => {
+    res.sendFile(path.join(__dirname,'/views/registro.html'));
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname,'/views/login.html'));
+});
